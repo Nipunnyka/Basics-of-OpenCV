10e14beebf669777773d97cc54224b36cd9c69ff
Nipunnyka <50680604+Nipunnyka@users.noreply.github.com>
2019-05-17 22:45:53 +0530

    Update P-Transform.cpp

--- P-Transform.cpp
+++ P-Transform.cpp
@@ -1,3 +1,20 @@
+/*
+                    PERSPECTIVE TRANSFORM
+   -This is not a linear transformation
+   -Makes use of warpPerspective funtion with 3x3 transform Matrix that performs the following operation
+   dst(x,y)=src((M00x+M01y+M02)/(M20x+M21y+M22),(M10x+M11y+M12)/(M20x+M21y+M22))
+   -new concepts learnt: point2f, point3f and CV_32FC1 data type
+   -warp perspective uses a helper matrix to plot 3d ideas onto a 2d plane
+   
+   Step 1: Read image
+   Step 2: Define the the input and output dimensions for the transformation
+   Step 3: Define the transformation Matrix
+   Step 4: Apply warpPerspective function
+   Step 5: display image
+   
+*/
+
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -22,10 +39,10 @@ int main(int argc, char *argv[])
     inquad[2]=Point2f(in.cols+15,in.rows+60);
     inquad[3]=Point2f(-40,  in.rows-5) ;
 
-    outquad[0]= Point2f(0,0);
-    outquad[1]=Point2f(in.cols-1, 0);
-    outquad[2]=Point2f(in.cols+1, in.rows-1);
-    outquad[3]=Point2f(0, in.rows-1);
+    outquad[0]= Point2f(0,0);                           //top left
+    outquad[1]=Point2f(in.cols-1, 0);                   //top right
+    outquad[2]=Point2f(in.cols+1, in.rows-1);           //bottom right
+    outquad[3]=Point2f(0, in.rows-1);                   //bottom left
 
     delta=getPerspectiveTransform(inquad, outquad);
 
