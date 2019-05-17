838855dc61fbbb622ae73be5388cf6b1e6cdce7d
Nipunnyka <50680604+Nipunnyka@users.noreply.github.com>
2019-05-17 08:30:32 +0530

    Face Detection using Haar Cascades

---
+++ face-detection.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Mat img = imread("arnold_schwarzenegger.jpg", CV_LOAD_IMAGE_COLOR);
+    if(img.empty())
+       return -1;
+    namedWindow("img", CV_WINDOW_AUTOSIZE );
+
+    CascadeClassifier fc_csd;
+    fc_csd.load("haarcascade_frontalface_alt.xml");
+
+    vector<Rect> faces;
+    fc_csd.detectMultiScale( img, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+    for(int i=0; i<faces.size(); i++)
+    {
+                Point center(faces[i].x+faces[i].width*0.5, faces[i].y+faces[i].height*0.5);
+                ellipse (img, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255), 4, 8, 0);
+    }
+
+    namedWindow("dface", CV_WINDOW_AUTOSIZE);
+
+    imshow("dface", img);
+
+    waitKey(0);
+    return 0;
+}
+
+
+
